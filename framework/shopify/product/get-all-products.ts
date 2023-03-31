ec98c9587baefe9cef7d072656f254890ebdf23f
Steven <uribgp@gmail.com>
2023-03-31 12:21:02 +0700

    error handling

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,27 +1,6 @@
+import fetchApi from "../utils/fetch-api"
+
 import getAllProductsQuery from "../utils/queries/get-all-products"
-
-const storeName = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN 
-const apiKey = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN
-
-type FetchParams = { 
-    query: string
-}
-
-const fetchApi = async ({ query }: FetchParams) => {
-    const url = storeName
-
-   const res = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-Shopify-Access-Token": apiKey
-        },
-        body: JSON.stringify({query})
-    })
-
-    const data = await res.json()
-    return { data }
-}
 
 const getAllProducts = async (): Promise<any[]> => {
     const products = await fetchApi({ query: getAllProductsQuery})
---
+++ framework/shopify/utils/fetch-api.ts
@@ -0,0 +1,29 @@
+const storeName = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN 
+const apiKey = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN
+
+type FetchParams = { 
+    query: string
+}
+
+const fetchApi = async ({ query }: FetchParams) => {
+    const url = storeName
+
+   const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Shopify-Access-Token": apiKey
+        },
+        body: JSON.stringify({query})
+    })
+
+    const {data, errors} = await res.json()
+
+    if (errors){
+        throw new Error(errors[0].message ?? errors.message)
+    }
+
+    return { data }
+}
+
+export default fetchApi
