0f74bbb794435bca1262a2a190102f7c92232487
Steven <uribgp@gmail.com>
2023-04-06 10:54:15 +0700

    product card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import type { InferGetStaticPropsType } from "next";
-import getAllproducts from "@framework/product/get-all-products";
+import getAllProducts from "@framework/product/get-all-products";
 import { getConfig } from "@framework/api/config";
 import { Layout } from "@components/common";
-
-export async function getStaticProps() {
-  const config = getConfig();
-  const products = await getAllproducts(config);
-
-  return {
-    props: {
-      products,
-    },
-    revalidate: 4 * 60 * 60,
-  };
-}
+import { ProductCard } from "@components/product";
 
 export default function Home({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>{JSON.stringify(products)}</div>;
+  return (
+    <div>
+      {products.slice(0, 3).map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const config = getConfig();
+  const products = await getAllProducts(config);
+  return {
+    props: { products },
+
+    // Revalidate searches for new products every 4 hours
+    revalidate: 4 * 60 * 60,
+  };
 }
 
 Home.Layout = Layout;
