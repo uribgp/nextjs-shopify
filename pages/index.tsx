d3468f48d6a9dce3523b91bdceba9db06b8c2eb5
Steven <uribgp@gmail.com>
2023-04-12 16:21:17 +0700

    marquee component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import getAllProducts from "@framework/product/get-all-products";
 import { getConfig } from "@framework/api/config";
 import { Layout } from "@components/common";
 import { ProductCard } from "@components/product";
-import { Grid, Hero } from "@components/ui";
+import { Grid, Hero, Marquee } from "@components/ui";
 
 export default function Home({
   products,
@@ -16,6 +16,11 @@ export default function Home({
         ))}
       </Grid>
       <Hero headline='Hello' description='description' />
+      <Marquee>
+        {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Marquee>
     </div>
   );
 }
