e836176dc83f1ce87e1fcd8930e4931959b5d857
Steven <uribgp@gmail.com>
2023-04-13 20:55:50 +0700

    layout b

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,11 @@ export default function Home({
           <ProductCard key={product.id} variant='slim' product={product} />
         ))}
       </Marquee>
+      <Grid layout='B'>
+        {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Grid>
     </div>
   );
 }
