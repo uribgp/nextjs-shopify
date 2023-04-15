1c51bacb46c237ecd3d34d283b35912c66a2df51
Steven <uribgp@gmail.com>
2023-04-15 15:09:17 +0700

    add footer

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import style from "./Layout.module.css";
+import Footer from "../Footer/Footer";
 
 type LayoutProps = {
   children: ReactNode;
@@ -9,6 +10,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={style.root}>
       <main className='fit'>{children}</main>
+      <Footer />
     </div>
   );
 };
