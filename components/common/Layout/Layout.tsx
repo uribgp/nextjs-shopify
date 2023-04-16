006fdf0dff1b7230dc5219186ca371ad392d57c2
Steven <uribgp@gmail.com>
2023-04-16 22:15:56 +0700

    navbar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from "react";
 import style from "./Layout.module.css";
-import Footer from "../Footer/Footer";
+import { Footer, Navbar } from "@components/common";
 
 type LayoutProps = {
   children: ReactNode;
@@ -9,6 +9,7 @@ type LayoutProps = {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={style.root}>
+      <Navbar />
       <main className='fit'>{children}</main>
       <Footer />
     </div>
