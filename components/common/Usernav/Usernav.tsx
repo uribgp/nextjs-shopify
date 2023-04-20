b523d38572abff63cb31d5cd18b03f6fe083efe2
Steven <uribgp@gmail.com>
2023-04-20 17:45:48 +0700

    wishlist

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -11,7 +11,7 @@ const Usernav: FC = () => {
           <Cart />
         </li>
         <li className={style.item}>
-          <Link href='/'>
+          <Link href='/wishlist'>
             <Heart />
           </Link>
         </li>
