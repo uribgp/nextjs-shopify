e702e2fd91a16c59a4529460a8fd67630dc57bba
Steven <uribgp@gmail.com>
2023-04-20 17:58:42 +0700

    sidebar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode } from "react";
 import style from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
 
 type LayoutProps = {
   children: ReactNode;
@@ -10,6 +12,9 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={style.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className='fit'>{children}</main>
       <Footer />
     </div>
