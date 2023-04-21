87d6263af089b829cb4b057930d6810ceab2f311
Steven <uribgp@gmail.com>
2023-04-21 17:51:46 +0700

    usememo to reduce rerenders

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,4 +1,4 @@
-import { FC, createContext, useContext, useReducer } from "react";
+import { FC, createContext, useContext, useReducer, useMemo } from "react";
 
 export interface StateModifiers {
   openSidebar: () => void;
@@ -48,11 +48,13 @@ export const UIProvider: FC<UIProviderProps> = ({ children }) => {
   const openSidebar = () => dispatch({ type: "OPEN_SIDEBAR" });
   const closeSidebar = () => dispatch({ type: "CLOSE_SIDEBAR" });
 
-  const value = {
-    ...state,
-    openSidebar,
-    closeSidebar,
-  };
+  const value = useMemo(() => {
+    return {
+      ...state,
+      openSidebar,
+      closeSidebar,
+    };
+  }, [state]);
 
   return <UIContext.Provider value={value}>{children}</UIContext.Provider>;
 };
