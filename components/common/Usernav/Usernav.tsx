2949ae3baeb73e6d288b5fd92d3cca5ff67b2a57
Steven <uribgp@gmail.com>
2023-04-21 16:39:59 +0700

    use uicontext to open sidebar

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -2,13 +2,16 @@ import { FC } from "react";
 import style from "./Usernav.module.css";
 import Link from "next/link";
 import { Bag as Cart, Heart } from "@components/icons";
+import { useUI } from "@components/ui/context";
 
 const Usernav: FC = () => {
+  const ui = useUI();
+
   return (
     <nav>
       <ul className={style.list}>
         <li className={style.item}>
-          <Cart />
+          <Cart onClick={ui.setIsSidebarOpen} />
         </li>
         <li className={style.item}>
           <Link href='/wishlist'>
