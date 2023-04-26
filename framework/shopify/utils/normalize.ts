bfa6290133867130982bc1de9367a7a82983edc2
Steven <uribgp@gmail.com>
2023-04-26 17:27:40 +0700

    normalize shopify options

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,5 +1,5 @@
-import { ImageEdge, MoneyV2, Product as ShopifyProduct } from "../schema";
-import { Product } from "@common/types/product";
+import { ImageEdge, MoneyV2, ProductOption, Product as ShopifyProduct } from "../schema";
+import { Product } from "@common/types/product"
 
 function normalizeProductImages({edges}: {edges: Array<ImageEdge>}){
     return edges.map(({node: {originalSrc: url, ...rest}}) => {
@@ -17,6 +17,27 @@ function normalizeProductPrice({currencyCode, amount}: MoneyV2){
     }
 }
 
+const normalizeProductOption = ({id, values, name: displayName}: ProductOption) => {
+    const normalized = {
+        id, displayName, values: values.map(value => {
+            let output: any = {
+                label: value
+            }
+
+            if (displayName.match(/colou?r/gi)) {
+                output = {
+                  ...output,
+                  hexColor: value
+                }
+              }
+
+            return output
+        })
+    }
+
+    return normalized
+}
+
 export function normalizeProduct(productNode: ShopifyProduct): Product {
     const { 
         id, 
@@ -26,6 +47,7 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
         description, 
         images: imageConnection,
         priceRange,
+        options,
         ...rest 
     } = productNode
 
@@ -38,6 +60,9 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
         slug: handle ? handle.replace(/^\/+|\/+$/g, "") : null, 
         images: normalizeProductImages(imageConnection),
         price: normalizeProductPrice(priceRange.minVariantPrice),
+        options: options ?
+        options.filter(option => option.name !== "Title")
+               .map(option => normalizeProductOption(option)) : [],
         ...rest
     }
 
