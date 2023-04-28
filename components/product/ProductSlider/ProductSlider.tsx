2b5e1e73607ac75d260c934a6cd4a36515eda793
Steven <uribgp@gmail.com>
2023-04-28 09:53:49 +0700

    slider arrows

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,20 +1,37 @@
-import React, { FC, Children, isValidElement } from "react";
+import React, { FC, Children, isValidElement, useState } from "react";
 import style from "./ProductSlider.module.css";
 import { useKeenSlider } from "keen-slider/react";
+import cn from "classnames";
 
 interface ProductSliderProps {
   children: React.ReactNode;
 }
 
 const ProductSlider: FC<ProductSliderProps> = ({ children }) => {
-  const [sliderRef, _] = useKeenSlider({
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
     initial: 0,
     loop: true,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
   });
 
   return (
     <div className={style.root}>
       <div ref={sliderRef} className='keen-slider h-full transition-opacity'>
+        <button
+          className={cn(style.leftControl, style.control)}
+          onClick={(e) => instanceRef.current?.prev()}
+        />
+        <button
+          onClick={(e) => instanceRef.current?.next()}
+          className={cn(style.rightControl, style.control)}
+        />
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
             return {
