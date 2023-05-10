6ff1947c81b8d554207c3b183e2f77b159b68f3a
Steven <uribgp@gmail.com>
2023-05-10 15:27:07 +0700

    normalize cart

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -1,5 +1,9 @@
 import useCart from "@common/cart/use-cart"
-import { createCheckout, getCheckoutQuery } from "@framework/utils"
+import {
+  checkoutToCart,
+  createCheckout,
+  getCheckoutQuery
+} from "@framework/utils"
 import { useMemo } from "react"
 
 export default useCart
@@ -21,7 +25,8 @@ export const handler = {
       checkout = await createCheckout(fetch)
     }
 
-    return checkout
+    const cart = checkoutToCart(checkout)
+    return cart
   },
   useHook: ({useData}: any) => {
     const data = useData({
--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,5 +1,65 @@
-import { ImageEdge, MoneyV2, ProductOption, ProductVariantConnection, Product as ShopifyProduct, SelectedOption } from "../schema";
+import { Checkout, 
+    CheckoutLineItemEdge,
+    ImageEdge, 
+    MoneyV2, 
+    ProductOption, 
+    ProductVariantConnection, 
+    Product as ShopifyProduct, SelectedOption } 
+    from "../schema";
 import { Product } from "@common/types/product"
+import { Cart } from "@common/types/cart"
+
+export const normalizeCart = (checkout: Checkout): Cart => {
+  return {
+    id: checkout.id,
+    createdAt: checkout.createdAt,
+    currency: {
+      code: checkout.totalPriceV2.currencyCode
+    },
+    taxesIncluded: checkout.taxesIncluded,
+    lineItemsSubtotalPrice: +checkout.subtotalPriceV2.amount,
+    totalPrice: checkout.totalPriceV2.amount,
+    lineItems: checkout.lineItems.edges.map(normalizeLineItem),
+    discounts: []
+  }
+}
+
+const normalizeLineItem = ({
+    node: { id, title, variant, ...rest}
+  }: CheckoutLineItemEdge): any => {
+    return {
+      id,
+      variantId: String(variant?.id),
+      productId: String(variant?.id),
+      name: title,
+      path: variant?.product?.handle ?? "",
+      discounts: [],
+      options: variant?.selectedOptions.map(({name, value}: SelectedOption) => {
+        const option = normalizeProductOption({
+          id,
+          name,
+          values: [value]
+        })
+  
+        return option
+      }),
+      variant: {
+        id: String(variant?.id),
+        sku: variant?.sku ?? "",
+        name: variant?.title,
+        image: {
+            url: process.env.NEXT_PUBLIC_FRAMEWORK === "shopify_local" ?
+              `/images/${variant?.image?.originalSrc}` :
+              variant?.image?.originalSrc ?? "/product-image-placeholder.svg"
+          },
+        requiresShipping: variant?.requiresShipping ?? false,
+        price: variant?.priceV2.amount,
+        listPrice: variant?.compareAtPriceV2?.amount,
+      },
+      ...rest
+    }
+  }
+  
 
 function normalizeProductImages({edges}: {edges: Array<ImageEdge>}){
     return edges.map(({node: {originalSrc: url, ...rest}}) => {
