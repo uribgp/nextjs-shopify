115d92430ee8fc6374a034ab62ffdebe4a57ecce
Steven <uribgp@gmail.com>
2023-05-14 16:12:02 +0700

    cart item component

--- components/cart/CartSidebar/CartSidebar.tsx
+++ components/cart/CartSidebar/CartSidebar.tsx
@@ -5,11 +5,11 @@ import { useUI } from "@components/ui/context";
 import useCart from "@framework/cart/use-cart";
 import { Button } from "@components/ui";
 import { LineItem } from "@common/types/cart";
+import CartItem from "../CartItem";
 
 const CartSidebar: FC = () => {
   const { closeSidebar } = useUI();
-  const { data } = useCart();
-  const isEmpty = (data?.lineItems.length ?? 0) <= 0;
+  const { data, isEmpty } = useCart();
 
   const rootClass = cn("h-full flex flex-col", {
     "bg-secondary text-secondary": isEmpty,
@@ -50,9 +50,11 @@ const CartSidebar: FC = () => {
             </h2>
             <ul className='py-6 space-y-6 sm:py-0 sm:space-y-0 sm:divide-y sm:divide-accents-3 border-t border-accents-3'>
               {data?.lineItems.map((item: LineItem) => (
-                <div key={item.id}>
-                  {item.name} - {item.quantity}
-                </div>
+                <CartItem
+                  key={item.id}
+                  item={item}
+                  currencyCode={data.currency.code}
+                />
               ))}
             </ul>
           </div>
--- framework/common/types/hooks.ts
+++ framework/common/types/hooks.ts
@@ -52,6 +52,9 @@ export type UseDataContext = {
 
 export type UseData<Data> = (context: UseDataContext) => Data
 
+export type SWRHookResponse<Data> =
+  SWRResponse<Data, any> & { isEmpty: boolean }
+
 export type SWRHook<H extends HookDescriptor = any> = {
     fetcherOptions: HookFetcherOptions
     fetcher: HookFetcherFn<
@@ -61,10 +64,9 @@ export type SWRHook<H extends HookDescriptor = any> = {
     >
     useHook(
         context: {
-            useData: UseData<SWRResponse<H["data"], any>>
+            useData: UseData<SWRHookResponse<H["data"]>>
           }
-          ): () => SWRResponse<H["data"], any>
+          ): () => SWRHookResponse<H["data"]>
   }
 
   export type Hook = MutationHook | SWRHook
-  
